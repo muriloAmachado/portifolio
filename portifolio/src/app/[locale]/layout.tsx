2fa728ab2922ba81f9bfee33be29fbb5e1b282d1
muriloAmachado <muriloandrademachado@gmail.com>
2025-08-28 21:21:49 -0300

    ajuste para build

--- portifolio/src/app/[locale]/layout.tsx
+++ portifolio/src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Bebas_Neue } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
